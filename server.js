28710255d3cca14a7c7882b458dd51df1888c198
Gabriel Sousa <gabrieldesousa05@gmail.com>
2021-06-05 21:15:41 -0300

    :rocket: Meu primeiro deploy no Heroku!

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const { APP_BASE_HREF } = require('@angular/common');
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/prisme'));
+app.use(express.static(`${__dirname}/dist/prisme`));
 
-app.get('*/', (req, res)=> {
-    res.sendFile(__dirname + '/dist/prisme/index.html');
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/prisme/index.html`));
 });
 
-app.listen(PORT, () => {
-    console.log('Servidor iniciado na porta ' + PORT);
-});
+app.listen(process.env.PORT || 8080);
